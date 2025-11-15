2bb3c5351f1071a67acb5730ff5b26584bf64cc7
Phil Massyn <phil.massyn@icloud.com>
2025-11-16 09:09:34 +1000

    Update

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import logging
 
 def main(target):
     os.makedirs(target,exist_ok=True)
-    with open(f"{target}/index.html") as q:
+    with open(f"{target}/index.html",'wt',encoding='utf-8') as q:
         q.write('Hello World')
 
 if __name__ == '__main__':
